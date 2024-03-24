b101319949ea2f588e34395451ac0bb248b1512e
inisseini <issei.kurata819@gmail.com>
2024-03-24 05:25:24 +0000

    test

--- src/react-components/home/HomePage.js
+++ src/react-components/home/HomePage.js
@@ -22,6 +22,7 @@ import { isHmc } from "../../utils/isHmc";
 import maskEmail from "../../utils/mask-email";
 
 export function HomePage() {
+  console.log('test succeed!')
   const auth = useContext(AuthContext);
   const intl = useIntl();
 
